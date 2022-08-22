681ca57884c402cef8fd2ea8b9ee64a4a857337d
OceanShape <oceanshape700@gmail.com>
2022-08-23 03:57:09 +0900

    feature: 스토리보드로 기본 UI구성(커스텀 뷰 추가)

---
+++ MVVM-example/MVVM-example/CustomView.swift
@@ -0,0 +1,29 @@
+import Foundation
+import UIKit
+
+class CustomView: UIView {
+    
+    @IBOutlet weak var valueLabel: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadView()
+    }
+    
+    private func loadView() {
+        let view = Bundle.main.loadNibNamed("CustomView",
+                                       owner: self,
+                                       options: nil)?.first as! UIView
+        view.frame = bounds
+        addSubview(view)
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        self.valueLabel.text = String(Int(sender.value))
+    }
+}
---
+++ MVVM-example/MVVM-example/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var customView: CustomView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
