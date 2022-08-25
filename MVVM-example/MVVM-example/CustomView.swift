e61f9ab89a1929297b4cad8d64dfcdfbb0d1d3e0
OceanShape <oceanshape700@gmail.com>
2022-08-26 00:48:33 +0900

    feature: 데이터 요소 추가(학년 + 반)

--- MVVM-example/MVVM-example/CustomView.swift
+++ MVVM-example/MVVM-example/CustomView.swift
@@ -4,6 +4,10 @@ import UIKit
 class CustomView: UIView {
 
     @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBOutlet weak var gradeNumberSlider: UISlider!
+    
+    @IBOutlet weak var classNumberSlider: UISlider!
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,10 +25,15 @@ class CustomView: UIView {
                                        options: nil)?.first as! UIView
         view.frame = bounds
         addSubview(view)
-        self.valueLabel.text = "0"
+        self.valueLabel.text = "2학년 8반"
     }
     
-    @IBAction func sliderValueChanged(_ sender: UISlider) {
-        self.valueLabel.text = String(Int(sender.value))
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        let gradeNumber = Int(gradeNumberSlider.value)
+        let classNumber = Int(classNumberSlider.value)
+        gradeNumberSlider.value = Float(gradeNumber)
+        classNumberSlider.value = Float(classNumber)
+        
+        self.valueLabel.text = String(format: "%d학년 %d반", gradeNumber, classNumber)
     }
 }
